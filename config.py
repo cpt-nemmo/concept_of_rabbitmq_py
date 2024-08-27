cc9a59a2d2a6889496f6cf62b2ec5d633b976d17
cpt-nemmo <ysfedoseev@yandex.ru>
2024-08-27 16:36:21 +0300

    set up config

---
+++ config.py
@@ -0,0 +1,18 @@
+import pika
+
+RMQ_HOST = 'localhost'
+RMQ_PORT = 5672
+RMQ_USER = 'guest'
+RMQ_PASSWORD = 'guest'
+MQ_EXCHANGE = ''
+MQ_ROUTING_KEY = 'news'
+
+conn_params = pika.ConnectionParameters(
+    host=RMQ_HOST,
+    port=RMQ_PORT,
+    credentials=pika.PlainCredentials(RMQ_USER, RMQ_PASSWORD),
+)
+
+
+def get_conn() -> pika.BlockingConnection:
+    return pika.BlockingConnection(conn_params)
